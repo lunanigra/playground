2a1fd9351557aa2eda2b4289032b01ad19ee89e3
JC <haddorp@lunanigra.de>
2015-01-25 11:36:43 +0100

    update after integration test

--- VendingMachine/vending/vending.ino
+++ VendingMachine/vending/vending.ino
@@ -1,13 +1,25 @@
-#define DEFAULT_TIME 2000
+#define DEFAULT_TIME 2500
+
+const int ROWS[] = {37, 36, 35, 34, 33, 31};
+const int COLS[] = {38, 39, 40, 41, 42, 43, 44, 45};
 
 void setup() {
   Serial.begin(57600);
 
   pinMode(13, OUTPUT);
 
-  for (int i = 30; i <= 45; i++) {
+  /* for (int i = 30; i <= 45; i++) {
     pinMode(i, OUTPUT);
     digitalWrite(i, HIGH);
+  } */
+  
+  for (int i = 0; i < 6; i++) {
+    pinMode(ROWS[i], OUTPUT);
+    digitalWrite(ROWS[i], HIGH);
+  }
+  for (int i = 0; i < 8; i++) {
+    pinMode(COLS[i], OUTPUT);
+    digitalWrite(COLS[i], HIGH);
   }
 
   Serial.println("Vending machine ready.");
@@ -27,8 +39,12 @@ void loop() {
   }
 
   if (current != "" && millis() > blinkStart + onTime) {
-    for (int i = 30; i <= 45; i++) {
-      digitalWrite(i, HIGH);
+    for (int i = 0; i < 6; i++) {
+    pinMode(ROWS[i], OUTPUT);
+      digitalWrite(ROWS[i], HIGH);
+    }
+    for (int i = 0; i < 8; i++) {
+      digitalWrite(COLS[i], HIGH);
     }
     digitalWrite(13, LOW);
     current = "";
@@ -45,17 +61,15 @@ void parseCommand() {
     action = command.substring(0, idx);
   }
 
-  if (current == "" && action.length() == 2 && action.charAt(0) >= 'A' && action.charAt(0) <= 'E' && action.charAt(1) >= '1' && action.charAt(1) <= '8') {
+  if (current == "" && action.length() == 2 && action.charAt(0) >= 'A' && action.charAt(0) <= 'F' && action.charAt(1) >= '1' && action.charAt(1) <= '8') {
     current = action;
     onTime = DEFAULT_TIME;
     if (idx != -1) {
       onTime = command.substring(idx + 1).toInt();
     }
 
-    // digitalWrite(action.charAt(0) - 35, LOW);
-    digitalWrite(-1 * (action.charAt(0) - 102), LOW);
-    digitalWrite(action.charAt(1) - 11, LOW);
-    // digitalWrite(-1 * (action.charAt(1) - 94), LOW);
+    digitalWrite(ROWS[action.charAt(0) - 65], LOW);
+    digitalWrite(COLS[action.charAt(1) - 49], LOW);
     
     digitalWrite(13, HIGH);
     blinkStart = millis();
